20659bf90c97bda204e293ba7f3ebdb6a500c49e
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2023-12-07 22:52:43 -0800

    First commit of working CPP code

---
+++ Day1/advent-day1.cpp
@@ -0,0 +1,19 @@
+/**
+ * Cesar Guerrero
+ * 12/7/2023
+ * 
+ * Advent of Code 2023 - Day 1
+ * 
+ * NOTE: I want to learn C++ and I figured doing Advent of Code would be a fun way to do this
+*/
+
+#include <iostream> //Header Library used for input and output in C++
+using namespace std; //Use names for objects and variables from the standard library
+
+//Entry into our program
+int main(){
+
+    cout << "Hello Advent Day 1\n";
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent 2023
+Link: https://adventofcode.com/2023/
