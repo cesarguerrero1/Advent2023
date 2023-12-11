a98ac0b18099de6513c811a2c39847f68ba14894
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2023-12-11 13:14:46 -0800

    Code correctly produced the sum for the challenge

--- Day1/advent-day1.cpp
+++ Day1/advent-day1.cpp
@@ -8,12 +8,43 @@
 */
 
 #include <iostream> //Header Library used for input and output in C++
+#include <fstream> //Header Library used for file input and output in C++
+#include <cstdio> //Used for printf
+
 using namespace std; //Use names for objects and variables from the standard library
 
 //Entry into our program
 int main(){
 
-    cout << "Hello Advent Day 1\n";
+    int sum = 0;
+    string line;
+
+    //The first thing we have to do is open the file
+    fstream file("calibration.txt", ios::in);
+
+    //Loop over each line of the file
+    while(getline(file, line)){
+        char temp[10];
+        int index = 0;
+        string num;
+        //We don't want to interact with the non-numeric characters
+        for(int i = 0; i < line.length(); i++){
+            char character = line[i];
+            if(character >= 48 && character <= 57){
+                temp[index] = character;
+                index++;
+            }
+        }
+        num += temp[0];
+        num += temp[index-1];
+        cout << "Number: " << num << "\n";
+
+        sum += stoi(num);
+    }
+    printf("Sum: %d\n", sum);
+
+    //Always close your files
+    file.close();
 
     return 0;
 }
