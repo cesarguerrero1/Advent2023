4d17cb850ba72a63a6bc088c9e844eaae5e8ff23
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2024-03-26 12:43:24 -0700

    Finisehd part one of Day 2

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,37 +1,167 @@
 /**
  * Cesar Guerrero
- * 01/09/2024
+ * 03/26/2024
  * 
- * Day 2: Advent Calendar 2023
- * 
- * NOTE: To solve Part 2 rather than refactor the program or start all over, I just reused the same code from verifySet and added a few lines to analyzeSet
+ * Day 2 - Cube Conundrum
+ *
 */
 
 #include <iostream>
 #include <string>
-//fstream is a library for reading and writing files
 #include <fstream>
-//The reason we include this is we want to treat a string as a stream.
 #include <sstream>
-//To make life easier we are going to use a hash-map
-#include <map>
+#include <unordered_map>
 
-//File
-std::string puzzleFile = "puzzle-input.txt";
 
-//These are the set of cubes that each game is played with
-std::map<std::string, int> cubes = {
-    {"red", 12},
-    {"green", 13},
-    {"blue", 14}
+//Function Prototypes
+int solutionPartOne(std::ifstream& file);
+int solutionPartTwo(std::ifstream& file);
+bool verifySet(const std::string& set, std::unordered_map<std::string, int>& cubes);
+
+
+/**
+ * Entry point for the program
+ * @return {int} - 0 if successful
+*/
+int main(){
+
+    //Intake our file
+    std::ifstream file("puzzle-input.txt");
+    if(file.fail()){
+        std::cerr << "Error opening file\n";
+        return 1;
+    }
+
+    if(solutionPartOne(file) != 0){
+        std::cerr << "Failed Part 1\n";
+        file.close();
+        return 2;
+    }
+
+    //Reset the file - We need to move the file pointer back to the beginning
+    file.clear();
+    file.seekg(0, std::ios::beg);
+    if(solutionPartTwo(file) != 0){
+        std::cerr << "Failed Part 2\n";
+        file.close();
+        return 3;
+    }
+
+    file.close();
+    return 0;
+}
+
+
+
+int solutionPartOne(std::ifstream& file){
+
+    //These are the set of cubes that each game is played with
+    std::unordered_map<std::string, int> cubes = {
+        {"red", 12},
+        {"green", 13},
+        {"blue", 14}
+    };
+
+    std::string line;
+    std::stringstream ss;
+    std::string set;
+    bool validGame;
+    int gameIDSum = 0;
+    int gameID = 1;
+    while(std::getline(file, line)){
+        //We only want after the first colon
+        int position = line.find(':');
+        if(position == std::string::npos){
+            std::cerr << "Error: File format error\n";
+            return 1;
+        }
+        line = line.substr(position + 1);
+
+        //We now need to start tokenizing on the semicolons
+        ss.clear();
+        ss.str(line);
+        validGame = true;
+        while(std::getline(ss, set, ';')){
+            if(!verifySet(set, cubes)){
+                std::cout << "Game " << gameID << " is invalid\n";
+                validGame = false;
+                break;
+            }
+        }
+
+        if(validGame){
+            gameIDSum += gameID;
+        }
+        gameID++;
+    }
+    
+    std::cout << "Sum of Game IDs: " << gameIDSum << "\n";
+
+    return 0;
+}
+
+int solutionPartTwo(std::ifstream& file){
+
+    std::unordered_map<std::string, int> minimumCubes = {
+        {"red", 0},
+        {"green", 0},
+        {"blue", 0}
+    };
+
+        /*
+
+
+        //We can now analyze each set of a game
+        validGame = true;
+        while(std::getline(fileLine, set, ';')){
+
+            //We are adding new functionality to see how to make the game valid
+            analyzeSet(set);
+        }
+    */
+
+    return 0;
+}
+
+
+/**
+ * This function will verify that the set is valid
+ * @param {std::string} set - The set to be analyzed
+ * @return {bool} - True if the set is valid, false otherwise
+*/
+bool verifySet(const std::string& set, std::unordered_map<std::string, int>& cubes){
+    if(set.empty()){
+        std::cerr << "Error: Empty set\n";
+        return false;
+    }
+
+    //We need to further tokenize on the commas
+    std::stringstream ss(set);
+    std::string cubesShown;
+    while(std::getline(ss, cubesShown, ',')){
+        //We know that the format is "N color"
+        int cubeColorPos = cubesShown.find_first_of("rgb");
+        if(cubeColorPos == std::string::npos){
+            std::cerr << "Error: Invalid cube color\n";
+            return false;
+        }
+
+        //Recall that substr takes in the starting position and an optional length
+        int cubeCount = std::stoi(cubesShown.substr(0, cubeColorPos-1));
+        std::string cubeColor = cubesShown.substr(cubeColorPos);
+
+        //Now we check against the map
+        if(cubeCount > cubes[cubeColor]){
+            return false;
+        }
+    }
+    return true;
 };
 
-std::map<std::string, int> minimumCubes = {
-    {"red", 0},
-    {"green", 0},
-    {"blue", 0}
-};
 
+
+
+/*
 //Function prototypes
 bool verifySet(std::string);
 void analyzeSet(std::string);
@@ -39,20 +169,10 @@ void analyzeSet(std::string);
 //Entry point for the program
 int main(){
 
-    //Open the file
-    std::ifstream file;
-    file.open(puzzleFile);
-
-    if(file.fail()){
-        std::cout << "Error opening file\n";
-        return 1;
-    }
-
     //We want to read the file line by line, and tokenize each line as we go
     std::string fileLineEntry;
     std::istringstream fileLine;
     std::string set;
-    int gameIDSum = 0;
     bool validGame;
     int count = 1;
     int gamePowerSum = 0;
@@ -63,40 +183,6 @@ int main(){
         minimumCubes["red"] = 0;
         minimumCubes["green"] = 0;
         minimumCubes["blue"] = 0;
-
-        //Remove everything up to and including the colon in each line
-        int position = fileLineEntry.find(':');
-        if(position == std::string::npos){
-            std::cout << "Error: File format error\n";
-            return 2;
-        }
-        fileLineEntry = fileLineEntry.substr(position + 1);
-
-        //We need to clear the stream so that we can use it again
-        fileLine.clear();
-        //Initialize the stream with the new string
-        fileLine.str(fileLineEntry);
-
-        //We can now analyze each set of a game
-        validGame = true;
-        while(std::getline(fileLine, set, ';')){
-
-            //We are adding new functionality to see how to make the game valid
-            analyzeSet(set);
-
-            //Only analyze the set if the game is still in a valid state
-            if(validGame == true && verifySet(set) == false){
-                //There is no point in further analyzing this game as a set is invalid
-                validGame = false;
-                std::cout << "Game " << count << " is invalid\n";
-            }
-        }
-
-        if(validGame == true){
-            std::cout << "Game " << count << " is valid\n";
-            gameIDSum += count;
-        }
-        count ++;
         
         //Print out the minimum number of cubes needed to make the game valid
         gamePowerSum += minimumCubes["red"] * minimumCubes["green"] * minimumCubes["blue"];
@@ -110,12 +196,14 @@ int main(){
 
     return 0;
 }
+*/
 
 
 /**
  * This function will analyze the set to determine the minimum number of cubes needed to make the set valid
  * @param {std::string} set - The set to be analyzed
 */
+/*
 void analyzeSet(std::string set){
     if(set.empty()){
         std::cout << "Error: Empty set\n";
@@ -141,37 +229,4 @@ void analyzeSet(std::string set){
     }
     return;
 }
-
-
-
-/**
- * This function will verify that the set is valid
- * @param {std::string} set - The set to be analyzed
- * @return {bool} - True if the set is valid, false otherwise
 */
-bool verifySet(std::string set){
-    if(set.empty()){
-        std::cout << "Error: Empty set\n";
-        return false;
-    }
-
-    std::istringstream setLine;
-    setLine.clear();
-    setLine.str(set);
-    std::string cubesShown; 
-    while(std::getline(setLine, cubesShown, ',')){
-        //We now have cube information
-        int cubeColorPos = cubesShown.find_first_of("rgb");
-
-        //Extract our information
-        std::string cubeColor = cubesShown.substr(cubeColorPos, cubesShown.length()-1);
-        int numberCubes = std::stoi(cubesShown.substr(0, cubeColorPos-1));
-
-        //Check against the map
-        if(numberCubes > cubes[cubeColor]){
-            std::cout << "Error: The set indicated " << numberCubes << " " << cubeColor << " cubes, but only " << cubes[cubeColor] << " are available\n";
-            return false;
-        }
-    }
-    return true;
-}
